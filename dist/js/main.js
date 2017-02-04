886bcf732a46805d531e9e7febe2deae6075bbcd
Artyom Guryanov <guryanov.artyom@gmail.com>
2017-02-04 20:27:53 +0600

    base setup; loaders

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,55 +1,8 @@
-/******/ (function(modules) { // webpackBootstrap
-/******/ 	// The module cache
-/******/ 	var installedModules = {};
-
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-
-/******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId])
-/******/ 			return installedModules[moduleId].exports;
-
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = installedModules[moduleId] = {
-/******/ 			exports: {},
-/******/ 			id: moduleId,
-/******/ 			loaded: false
-/******/ 		};
-
-/******/ 		// Execute the module function
-/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-
-/******/ 		// Flag the module as loaded
-/******/ 		module.loaded = true;
-
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-
-
-/******/ 	// expose the modules object (__webpack_modules__)
-/******/ 	__webpack_require__.m = modules;
-
-/******/ 	// expose the module cache
-/******/ 	__webpack_require__.c = installedModules;
-
-/******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
-
-/******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(0);
-/******/ })
-/************************************************************************/
-/******/ ([
+webpackJsonp([0,1],[
 /* 0 */
 /***/ function(module, exports) {
 
-	/* eslint-env browser */
-
-	// Imports
-
-	// Code
-	"use strict";
+	eval("/* eslint-env browser */\n\n// Imports\n\n// Code\n\"use strict\";//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiMC5qcyIsInNvdXJjZXMiOlsid2VicGFjazovLy9zcmMvanMvbWFpbi5qcz82OTFmIl0sInNvdXJjZXNDb250ZW50IjpbIi8qIGVzbGludC1lbnYgYnJvd3NlciAqL1xuXG4vLyBJbXBvcnRzXG5cbi8vIENvZGVcblxuXG4vLyBXRUJQQUNLIEZPT1RFUiAvL1xuLy8gc3JjL2pzL21haW4uanMiXSwibWFwcGluZ3MiOiJBQUFBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7Iiwic291cmNlUm9vdCI6IiJ9");
 
 /***/ }
-/******/ ]);
+]);
--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,15 @@ const NODE_ENV = process.env.NODE_ENV || 'development';
 const webpack = require('webpack');
 
 module.exports = {
-  entry: './src/js/main.js',
+  context: __dirname + '/src',
+
+  entry: {
+    main: './js/main.js'
+  },
   output: {
-    filename: 'bundle.js'
+    path: __dirname + '/dist/js',
+    public: '/js/',
+    filename: '[name].js'
   },
 
   watch: NODE_ENV === 'development',
@@ -21,7 +27,65 @@ module.exports = {
 
   plugins: [
     new webpack.DefinePlugin({
-      NODE_ENV: NODE_ENV
+      NODE_ENV: JSON.stringify(NODE_ENV),
+      LANG: JSON.stringify("en")
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: "common"
     })
-  ]
+  ],
+
+  resolve: {
+    root: __dirname + '/src/js/vendor',
+    modulesDirectories: ['node_modules'],
+    extensions: ['', '.js']
+  },
+
+  resolveLoader: {
+    modulesDirectories: ['node_modules'],
+    moduleTemplates: ['*-loader', '*'],
+    extensions: ['', '.js']
+  },
+
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      include: __dirname + '/src/js',
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015', 'es2016'],
+        cacheDirectory: true,
+        plugins: ['transform-runtime']
+      }
+    }, {
+      test: /\.css$/,
+      include: __dirname + '/src/postcss',
+      loaders: [
+        'style-loader',
+        'css-loader?importLoaders=1',
+        'postcss-loader'
+      ]
+    }, {
+      test: /\.pug$/,
+      loader: 'pug-loader'
+    }, {
+      test: /\.twig$/,
+      loader: 'twig-loader'
+    }, {
+      test: /\.(png|jpg|svg|ttf|eot|woff|woff2)$/,
+      include: /\/node_modules\//,
+      loader: 'file?name=[1].[ext]&regExp=node_modules/(.*)'
+    }, {
+      test: /\.(png|jpg|svg|ttf|eot|woff|woff2)$/,
+      exclude: /\/node_modules\//,
+      loader: 'url?name=[path][name].[ext]&limit=4096'
+    }
+    ]
+  }
 };
+
+if (NODE_ENV == 'production') {
+  module.exports.plugins.push(
+    new webpack.optimize.UglifyJsPlugin()
+  );
+}
